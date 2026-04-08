7d99b854a0b443c7ba791424c34a059feac48ddd
rohit-7620 <rohit.salke.aids.2023@vpkbiet.org>
2026-04-08 10:58:42 +0530

    Added scaling in preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,8 +1,13 @@
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_iris
+
+
+from sklearn.preprocessing import StandardScaler
 
 def load_and_split_data():
     data = load_iris()
     X = data.data
     y = data.target
+
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+
     return train_test_split(X, y, test_size=0.2, random_state=42)
