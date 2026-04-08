a7212d9725b642b8bcc8bb89c06cb7dc4480eb47
rohit-7620 <rohit.salke.aids.2023@vpkbiet.org>
2026-04-08 11:12:14 +0530

    Resolved conflict and finalized model

--- model.py
+++ model.py
@@ -1,6 +1,6 @@
-from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
 
 def train_model(X_train, y_train):
-    model = LogisticRegression(max_iter=200)
+    model = RandomForestClassifier(n_estimators=100)
     model.fit(X_train, y_train)
     return model
